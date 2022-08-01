3a816b1d52357283d39190269914c7fee09271e0
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-01 15:12:19 +0100

    complete ticket 4 - add api/reviews endpoint and functionality/testing for getting reviews by id

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const {
-    categoryControllers: { getCategories }
+    categoryControllers: { getCategories },
+    reviewControllers: { getReviewById }
 } = require("./controllers/index");
 
 const express = require("express");
 const app = express();
 
 app.get("/api/categories", getCategories);
+
+app.get("/api/reviews/:review_id", getReviewById)
+
+app.use((err, req, res, next) => {
+    const { status, msg } = err;
+    res.status(status).send({ msg })
+})
 
 module.exports = app;
