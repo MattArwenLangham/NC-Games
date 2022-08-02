5a6975168dc7f5d5cb0d1635e7a3dbda4f99d016
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-02 11:27:19 +0100

    compelete ticket 7, adding a comment count property to the existing reviews

--- models/reviews.js
+++ models/reviews.js
@@ -7,8 +7,10 @@ exports.fetchReviewById = (review_id) => {
     }
 
     return db.query(`
-        SELECT * FROM reviews
-        WHERE review_id = $1
+        SELECT reviews.*, COUNT (comment_id) AS comment_count FROM reviews
+        LEFT JOIN comments ON reviews.review_id = comments.review_id
+        WHERE reviews.review_id = $1
+        GROUP BY reviews.review_id;
     `, [review_id]).then(({ rows: review }) => {
 
         if(!review.length){
