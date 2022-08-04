edab7ed94348478ff7f543bc899e7b5bb0f32577
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-04 12:34:06 +0100

    build heroku hosting app

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,16 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set')
 }
+
+const config = 
+  ENV === 'production' ? {
+    connectionString: process.env.DATABASE_URL,
+    SSL: {
+      rejectUnauthorized: false,
+    },
+  } : {}
 
 module.exports = new Pool();
