af20883420ffd44eca3e1b00b3bc4d107463354b
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-04 10:57:32 +0100

    complete ticket 12 - adds comments/:comment_id endpoint and DELETE HTTP method

--- models/comments.js
+++ models/comments.js
@@ -13,6 +13,23 @@ exports.insertComment = (body, review_id, username) => {
             ($1, $2, $3)
         RETURNING *;`, [body, review_id, username])
         .then(({ rows: [comment] }) => {
-            return comment;
+            return comment
         })
+}
+
+exports.removeCommentById = (comment_id) => {
+    
+    if(isNaN(parseInt(comment_id))){
+        return Promise.reject({status: 400, msg: 'Invalid comment_id type!'})
+    }
+
+    return db.query(`
+        DELETE FROM comments
+        WHERE comment_id = $1
+        RETURNING *`, [comment_id])
+    .then(({ rows: [commentRemoved]}) => {
+        if(!commentRemoved){
+            return Promise.reject({status: 404, msg: "Comment not found!"})
+        }
+    })
 }
