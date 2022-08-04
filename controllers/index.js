a17f8419e589d217a0129e4567b22a9b25e41827
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-04 14:03:32 +0100

    update /api/ to return endpoints

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,8 @@ exports.categoryControllers = require('./categories.js')
 exports.reviewControllers = require("./reviews.js")
 exports.userControllers = require("./users")
 exports.commentControllers = require("./comments")
+const endpoints = require("../endpoints.json")
+
+exports.getEndpoints = (req, res) => {
+    res.send(endpoints)
+}
