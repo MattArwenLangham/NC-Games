94ca8903d821b2d8fee5de8aba60f53ade2100ee
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-08-22 09:45:31 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const apiRouter = require('./routes/api.js')
 const express = require("express")
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', apiRouter)
 
